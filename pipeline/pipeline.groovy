c797d4d7230a6d379d5e8d60148a4fa2647f7d5d
ming <zxc81906119@gmail.com>
2025-04-03 20:54:01 +0800

    asfa

--- pipeline/pipeline.groovy
+++ pipeline/pipeline.groovy
@@ -1,47 +1,17 @@
-// Uses Declarative syntax to run commands inside a container.
-pipeline {
-    agent {
-        kubernetes {
-            // Rather than inline YAML, in a multibranch Pipeline you could use: yamlFile 'jenkins-pod.yaml'
-            // Or, to avoid YAML:
-            // containerTemplate {
-            //     name 'shell'
-            //     image 'ubuntu'
-            //     command 'sleep'
-            //     args 'infinity'
-            // }
-            yaml '''
-apiVersion: v1
-kind: Pod
-spec:
-  containers:
-  - name: shell
-    image: ubuntu
-    command:
-    - sleep
-    args:
-    - infinity
-    securityContext:
-      # ubuntu runs as root by default, it is recommended or even mandatory in some environments (such as pod security admission "restricted") to run as a non-root user.
-      runAsUser: 1000
-'''
-            // Can also wrap individual steps:
-            // container('shell') {
-            //     sh 'hostname'
-            // }
-            defaultContainer 'shell'
-            retries 2
-        }
-    }
-    stages {
-        stage('Main') {
-            steps {
+pipeline{
+    agent any
+
+    stages{
+
+        stage('test'){
+            steps{
+
                 sh 'id'
                 sh 'pwd'
-                sh 'ls -la'
-                sh 'echo "$HOME"'
                 sh 'hostname'
             }
         }
+
+
     }
 }
