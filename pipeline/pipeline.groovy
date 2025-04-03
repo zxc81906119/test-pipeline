eb56c00a5dd5a1e3150580aa81346db88b7634f8
ming <zxc81906119@gmail.com>
2025-04-03 20:56:12 +0800

    asfa

--- pipeline/pipeline.groovy
+++ pipeline/pipeline.groovy
@@ -1,11 +1,55 @@
-pipeline{
+pipeline {
     agent any
 
-    stages{
+    stages {
 
-        stage('test'){
-            steps{
+        stage('test') {
+            steps {
 
+                sh 'id'
+                sh 'pwd'
+                sh 'hostname'
+            }
+        }
+
+
+        stage('k8s') {
+
+            agent {
+                kubernetes {
+                    // Rather than inline YAML, in a multibranch Pipeline you could use: yamlFile 'jenkins-pod.yaml'
+                    // Or, to avoid YAML:
+                    // containerTemplate {
+                    //     name 'shell'
+                    //     image 'ubuntu'
+                    //     command 'sleep'
+                    //     args 'infinity'
+                    // }
+                    yaml '''
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: shell
+    image: ubuntu
+    command:
+    - sleep
+    args:
+    - infinity
+    securityContext:
+      # ubuntu runs as root by default, it is recommended or even mandatory in some environments (such as pod security admission "restricted") to run as a non-root user.
+      runAsUser: 1000
+'''
+                    // Can also wrap individual steps:
+                    // container('shell') {
+                    //     sh 'hostname'
+                    // }
+                    defaultContainer 'shell'
+                    retries 2
+                }
+            }
+
+            steps {
                 sh 'id'
                 sh 'pwd'
                 sh 'hostname'
